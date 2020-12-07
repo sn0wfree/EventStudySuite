404f3081020a7e1e0cd4176b95c28af8f9900d54
sn0wfree <snowfreedom0815@gmail.com>
2020-12-07 12:17:15 +0800

    add MarketModel Fast approach

--- EventStudySuite/model/MarketModel.py
+++ EventStudySuite/model/MarketModel.py
@@ -7,12 +7,51 @@ import statsmodels.api as sm
 from EventStudySuite.model import DefaultModel
 
 
+class MarketModelFast(DefaultModel):
+    @staticmethod
+    def cal_ar(estimation_df, event_df, stock: str, formula: str = "{stock} ~ 1 + {factors}", factors=['Mkt_RF'],
+               raise_error=False):
+        """
+
+        :param raise_error:
+        :param estimation_df:  pd.DataFrame
+        :param event_df:  pd.DataFrame
+        :param stock:
+        :param formula:
+        :param factors:
+        :return:
+        """
+        # f1 = formula.format(stock=stock, factors="+".join(factors))
+        y_array = estimation_df[stock]
+        w = 1 - y_array.isna() * 1
+        length = estimation_df.shape[0]
+        if w.sum() == length:
+            y_array = y_array.values.ravel()
+            x_array = estimation_df[factors[0]].values.ravel()
+            sum_y = np.sum(y_array)
+            sum_x = np.sum(x_array)
+            sum_xy = np.do(x_array, y_array.T)
+            sum_x2 = np.dot(x_array, x_array)
+            beta_Mkt = ((sum_x * sum_y) / length - sum_xy) / ((sum_x * sum_x) / length - sum_x2)
+            alpha = (sum_y - sum_x * beta_Mkt) / length
+            event_df[f'{stock}_er'] = np.dot(event_df[factors].values, beta_Mkt) + alpha
+            # event_df[f'{stock}_ar'] = event_df.eval(f'{stock} - {stock}_er')
+            series = event_df.eval(f'{stock} - {stock}_er')
+            series.index = event_df.index
+            return series
+        else:
+            series = pd.Series([None] * event_df.shape[0])
+            series.index = event_df.index
+            return series
+
+
 class MarketModel(DefaultModel):
     @staticmethod
     def cal_ar(estimation_df, event_df, stock: str, formula: str = "{stock} ~ 1 + {factors}", factors=['Mkt_RF'],
                raise_error=False):
         """
 
+        :param raise_error:
         :param estimation_df:  pd.DataFrame
         :param event_df:  pd.DataFrame
         :param stock:
@@ -21,24 +60,34 @@ class MarketModel(DefaultModel):
         :return:
         """
         f1 = formula.format(stock=stock, factors="+".join(factors))
-        # print(f1)
-        if estimation_df.dropna().shape[0] == estimation_df.shape[0]:
-            models = sm.OLS.from_formula(f1, data=estimation_df).fit()
-            params = models.params
-            beta_Mkt = params[factors]
-            alpha = params["Intercept"]
-            bse = models.bse
-            event_df[f'{stock}_er'] = np.dot(event_df[factors].values, beta_Mkt) + alpha
-            # event_df[f'{stock}_ar'] = event_df.eval(f'{stock} - {stock}_er')
-            return event_df.eval(f'{stock} - {stock}_er')
-        else:
-            if raise_error:
-                raise ValueError('estimation_df has nan data!')
-            else:
+        w = 1 - estimation_df[stock].isna() * 1
+        length = estimation_df.shape[0]
+        if w.sum() == length:
 
-                series = pd.Series([None] * event_df.shape[0])
+            # print(f1)
+            if estimation_df.dropna().shape[0] == estimation_df.shape[0]:
+                models = sm.OLS.from_formula(f1, data=estimation_df).fit()
+                params = models.params
+                beta_Mkt = params[factors]
+                alpha = params["Intercept"]
+                bse = models.bse
+                event_df[f'{stock}_er'] = np.dot(event_df[factors].values, beta_Mkt) + alpha
+                # event_df[f'{stock}_ar'] = event_df.eval(f'{stock} - {stock}_er')
+                series = event_df.eval(f'{stock} - {stock}_er')
                 series.index = event_df.index
                 return series
+            else:
+                if raise_error:
+                    raise ValueError('estimation_df has nan data!')
+                else:
+
+                    series = pd.Series([None] * event_df.shape[0])
+                    series.index = event_df.index
+                    return series
+        else:
+            series = pd.Series([None] * event_df.shape[0])
+            series.index = event_df.index
+            return series
 
 
 if __name__ == '__main__':
